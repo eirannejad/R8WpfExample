ad1e92e53b90d59e13b36aa1dfb3381c13d6cdd8
eirannejad <eirannejad@gmail.com>
2023-11-03 14:08:53 -0400

    silencing my ocd

--- ShowWindow.py
+++ ShowWindow.py
@@ -1,14 +1,14 @@
 #! python2
 """This script imports a XAML file and loads a UI"""
 
-import clr
+# the usual suspects
 import os.path as op
 
 import System
 import Rhino
-from Rhino.PlugIns import PlugIn
 
-clr.AddReference("IronPython.Modules")
+# import IronPython's WPF helper module
+import clr
 clr.AddReference("IronPython.Wpf")
 import wpf
 
@@ -16,25 +16,25 @@ import wpf
 class MyWindow(System.Windows.Window):
     def __init__(self):
         # for testing
-        xaml_file = r"C:\Users\ein\gits\rhino\src4\rhino4\Plug-ins\RhinoCodePlugins\tests_projects\wpf\Window.xaml"
+        xaml_file = op.join(op.dirname(__file__), "Window.xaml")
         # get path of xaml file from plugin location
-        plugin = op.dirname(PlugIn.PathFromName("WpfExample"))
+        plugin = Rhino.PlugIns.PlugIn.PathFromName("WpfExample")
         if plugin:
-            xaml_file = op.join(plugin, "shared/Window.xaml")
+            xaml_file = op.join(op.dirname(plugin), "shared/Window.xaml")
         
         # load xaml
         wpf.LoadComponent(self, xaml_file)
 
-        self.Title = "Custom Window"
-
         # make rhino window the owner of this window
         helper = System.Windows.Interop.WindowInteropHelper(self)
         helper.Owner = Rhino.RhinoApp.MainWindowHandle()
-        pass
+
+        # set some properties
+        self.Title = "Custom Window"
 
     def do_something(self, sender, args):
         Rhino.RhinoApp.WriteLine("Did Something!")
 
 
-
+# create and instance of window and show
 MyWindow().Show()
